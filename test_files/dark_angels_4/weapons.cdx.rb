c4532ec85550d81cfc31b47c6248dd25850d3478
Scott Conley <scott@definemystyle.com>
2010-11-18 01:34:32 -0500

    initial commit

---
+++ test_files/dark_angels_4/weapons.cdx.rb
@@ -0,0 +1,30 @@
+# weapons
+weapon :meltagun {
+  range: 12,
+  strength: 8,
+  ap: 1
+  type: "assault 1, melta"
+}
+weapon :assault_cannon
+weapon :autocannon
+weapon :bolt_pistol
+weapon :boltgun
+weapon :flamer
+weapon :heavy_bolter
+weapon :heavy_flamer
+weapon :lascannon
+weapon :missile_launcher
+weapon :multimelta
+weapon :plasma_cannon
+weapon :plasma_gun
+weapon :plasma_pistol
+weapon :shotgun
+weapon :sniper_rifle
+weapon :storm_bolter
+weapon :typhoon_missile
+weapon :demolisher
+weapon :whirlwind
+weapon :power_fist
+weapon :chainsword
+weapon :meltabombs
+weapon :crozius_arcanum
