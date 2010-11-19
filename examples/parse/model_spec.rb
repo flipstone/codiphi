bfaee88bed49830d6710d7002ee26c72860f9d58
Scott Conley <scranky@orangejello.local>
2010-11-19 18:10:35 -0500

    fixes for new line escaping problem /doh

--- examples/parse/model_spec.rb
+++ examples/parse/model_spec.rb
@@ -4,16 +4,16 @@ describe CodiphiParser, "model" do
   parser = CodiphiParser.new
 
   it "parses without attributes" do
-    parser.should parse "model :pants"
-    parser.should parse "model :pants\n"
-    parser.should parse " model   :pants"
-    parser.should parse " model   :notab   \n" 
-    parser.should parse " model   :tab   \n" # tab inlined
+    parser.should parse %{model :pants}
+    parser.should parse %{model :pants\n}
+    parser.should parse %{ model   :pants}
+    parser.should parse %{ model   :notab   \n} 
+    parser.should parse %{ model   :tab   \n} # tab inlined
   end
 
   it "parses with attributes" do
-    parser.should parse "model :pants { cost 100 }"
-    parser.should parse "  model   :pants  {   cost   100   }  "
+    parser.should parse %{model :pants { cost 100 }}
+    parser.should parse %{  model   :pants  {   cost   100   }  }
   end
 
 end
