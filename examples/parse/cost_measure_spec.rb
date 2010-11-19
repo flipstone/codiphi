742a9de8c1246b3e36c4d438842e1d83826cec18
Scott Conley <scranky@orangejello.local>
2010-11-19 18:09:02 -0500

    fixes for new line escaping problem /doh

--- examples/parse/cost_measure_spec.rb
+++ examples/parse/cost_measure_spec.rb
@@ -4,12 +4,19 @@ describe CodiphiParser, "cost_measure" do
   parser = CodiphiParser.new
 
   it "parses" do
-    parser.should parse 'cost_measure "pants"'
-    parser.should parse ' cost_measure "pants"'
+    parser.should parse %{"cost_measure "pants"}
+    parser.should parse %{ cost_measure "pants" }
+  end
+
+  it "parses end of line comment" do
+    parser.should parse %{cost_measure "pants"}
+    parser.should parse %{ cost_measure "pants"  # wooo ! }
+    parser.should parse %{cost_measure "pants"  # wooo ! \n}
+    parser.should parse %{ cost_measure "pants"  # wooo ! \n}
   end
   
   it "fails non string arguments" do
-    parser.should_not parse 'cost_measure :pants'
-    parser.should_not parse 'cost_measure pants'
+    parser.should_not parse %{cost_measure :pants}
+    parser.should_not parse %{cost_measure pants}
   end
 end
