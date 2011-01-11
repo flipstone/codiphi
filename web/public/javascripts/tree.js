c69927cb58bfa6d5ed4c57ff20276a8b0eed2f53
David Vollbracht <david.vollbracht@gmail.com>
2011-01-10 22:45:20 -0500

    Javascript for adding subtrees to trees

--- web/public/javascripts/tree.js
+++ web/public/javascripts/tree.js
@@ -4,34 +4,69 @@ $.widget('ui.tree', {
 
     this.tree_html = $('<ul class="branch"></ul>');
 
-    var form = $('<form action="#" class="add-node"></form>');
-    form.append('Name: <input type="text" name="name" class="name"/>');
-    form.append('Value: <input type="text" name="value" class="value"/>');
-    form.append('<input type="submit" value="Done", class="done"/>');
+    var node_form = $('<form action="#" class="add-node"></form>');
+    node_form.append('Name: <input type="text" name="name" class="name"/>');
+    node_form.append('Value: <input type="text" name="value" class="value"/>');
+    node_form.append('<input type="submit" value="Value", class="done"/>');
 
-    form.data('tree', this);
-    form.submit(function(e) {
+    node_form.data('tree', this);
+    node_form.submit(function(e) {
       e.preventDefault();
       node = {};
-      node[form.find('.name').val()] = form.find('.value').val();
-      form.data('tree').addNode(node);
+      node[node_form.find('.name').val()] = node_form.find('.value').val();
+      node_form.data('tree').addNode(node);
+    });
+
+    var branch_form = $('<form action="#" class="add-branch"></form>');
+    branch_form.append('Name: <input type="text" name="name" class="name"/>');
+    branch_form.append('<input type="submit" value="Sub-Tree", class="done"/>');
+
+    branch_form.data('tree', this);
+    branch_form.submit(function(e) {
+      e.preventDefault();
+      branch_form.data('tree').addBranch(branch_form.find('.name').val());
     });
 
     $(this.element).append(this.tree_html);
-    $(this.element).append(form);
+    $(this.element).append(node_form);
+    $(this.element).append(branch_form);
   },
 
   treeData: function() {
-    return this.data;
+    var treeData = {};
+
+    for (n in this.data) {
+      var v = this.data[n];
+
+
+      if (typeof(v) == "function") {
+        treeData[n] = v();
+      } else {
+        treeData[n] = v;
+      }
+    }
+
+    return treeData;
   },
 
   addNode: function(data) {
     $.extend(this.data, data);
     for(name in data) {
-      var leaf = $("<li class='leaf'><span class='name'></span><span class='value'</span></li>");
+      var leaf = $("<li class='leaf'><span class='name'></span><span class='value'></span></li>");
       leaf.find('.name').html(name);
       leaf.find('.value').html(data[name]);
       this.tree_html.append(leaf);
     }
+  },
+
+  addBranch: function(name) {
+    this.data[name] = function() {
+      return leaf.data('tree').treeData();
+    }
+
+    var leaf = $("<li class='leaf'><span class='name'></span></li>");
+    leaf.find('.name').html(name);
+    leaf.tree();
+    this.tree_html.append(leaf);
   }
 });
