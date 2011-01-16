c1d3edd3df61738abc3a6045f07fb68c4b70fa75
David Vollbracht <david.vollbracht@gmail.com>
2011-01-16 15:07:38 -0500

    Add lists to tree builder

--- web/public/javascripts/tree.js
+++ web/public/javascripts/tree.js
@@ -1,3 +1,49 @@
+$.widget('ui.list', {
+  _create: function() {
+    this.data = [];
+
+    this.tree_html = $('<ul class="list"></ul>');
+
+    var branch_form = $('<form action="#" class="add-branch"></form>');
+    branch_form.append('<input type="submit" value="List Elem", class="done"/>');
+
+    branch_form.data('list', this);
+    branch_form.submit(function(e) {
+      e.preventDefault();
+      branch_form.data('list').addBranch();
+    });
+
+    $(this.element).append(this.tree_html);
+    $(this.element).append(branch_form);
+  },
+
+  addBranch: function() {
+    this.data.push(function() {
+      return holder.data('tree').treeData();
+    });
+
+    var member = $("<li class='member'><div class='list-member-holder'></div></li>");
+    this.tree_html.append(member);
+    var holder = member.find('.list-member-holder');
+    holder.tree();
+  },
+
+  treeData: function() {
+    var treeData = [];
+
+    for (n in this.data) {
+      var v = this.data[n];
+
+      if (typeof(v) == "function") {
+        treeData.push(v());
+      } else {
+        treeData.push(v);
+      }
+    }
+    return treeData;
+  }
+});
+
 $.widget('ui.tree', {
   _create: function() {
     this.data = {};
@@ -27,9 +73,20 @@ $.widget('ui.tree', {
       branch_form.data('tree').addBranch(branch_form.find('.name').val());
     });
 
+    var list_form = $('<form action="#" class="add-list"></form>');
+    list_form.append('Name: <input type="text" name="name" class="name"/>');
+    list_form.append('<input type="submit" value="List", class="done"/>');
+
+    list_form.data('tree', this);
+    list_form.submit(function(e) {
+      e.preventDefault();
+      list_form.data('tree').addList(list_form.find('.name').val());
+    });
+
     $(this.element).append(this.tree_html);
     $(this.element).append(node_form);
     $(this.element).append(branch_form);
+    $(this.element).append(list_form);
   },
 
   treeData: function() {
@@ -67,6 +124,17 @@ $.widget('ui.tree', {
     var leaf = $("<li class='leaf'><span class='name'></span></li>");
     leaf.find('.name').html(name);
     leaf.tree();
+    this.tree_html.append(leaf);
+  },
+
+  addList: function(name) {
+    this.data[name] = function() {
+      return leaf.data('list').treeData();
+    }
+
+    var leaf = $("<li class='leaf'><span class='name'></span></li>");
+    leaf.find('.name').html(name);
+    leaf.list();
     this.tree_html.append(leaf);
   }
 });
