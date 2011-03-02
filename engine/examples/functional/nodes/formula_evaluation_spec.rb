9ebd56fb4443f089dc95616360e58fa24f043173
David Vollbracht <david.vollbracht@gmail.com>
2011-03-02 00:41:57 -0500

    Implement arithmetic for schematic formulas

---
+++ engine/examples/functional/nodes/formula_evaluation_spec.rb
@@ -0,0 +1,29 @@
+require_relative './spec_helper.rb'
+
+module Codiphi
+  describe "formula" do
+    def self.it_evals(formula, options)
+      expected = options[:to]
+      it "#{formula} evals to #{expected}" do
+        Codiphi::Formula::Parser.parse(formula).evaluate.should == expected
+      end
+    end
+
+    it_evals "1", to: 1
+    it_evals "1 + 3", to: 4
+    it_evals "1 - 3", to: -2
+    it_evals "2 * 3", to: 6
+    it_evals "6 / 2", to: 3
+    it_evals "2 + 3 + 4", to: 9
+    it_evals "2 + 3 - 4", to: 1
+    it_evals "2 - 3 + 4", to: 3
+    it_evals "2 + 3 * 4", to: 14
+    it_evals "2 * 3 + 4", to: 10
+    it_evals "5 + 6 / 2", to: 8
+    it_evals "6 / 3 + 1", to: 3
+    it_evals "3 + 6 / 3 + 1", to: 6
+
+    it_evals "(3 + 6) / 3 + 1", to: 4
+    it_evals "(3 + 6) / (2 + 1)", to: 3
+  end
+end
--- engine/lib/codiphi/nodes/literal_nodes.rb
+++ engine/lib/codiphi/nodes/literal_nodes.rb
@@ -11,6 +11,14 @@ module Codiphi
     def text_value
       super.to_i
     end
+
+    def evaluate
+      text_value
+    end
+
+    def left_associative_evaluate(left_assoc_value, operator)
+      operator.operate(left_assoc_value, evaluate)
+    end
   end
 
   class NameNode < CodiphiNode
